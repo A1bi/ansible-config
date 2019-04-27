b7406d6d638bbe334f002982a628428f2fff755a
Albrecht Oster <albrecht@oster.online>
2019-04-27 20:24:30 +0200

    add nsd role

--- filter_plugins/dicts.py
+++ filter_plugins/dicts.py
@@ -7,9 +7,16 @@ def pluck(collection, key, default=None):
 def any_with_attr(collection, key):
   return len(list(filter(lambda el: el.get(key, False), collection))) > 0
 
+def select_attr(collection, key, value, default):
+  if default == None:
+    return [el for el in collection if el[key] == value]
+  else:
+    return [el for el in collection if el.get(key, default) == value]
+
 class FilterModule(object):
   def filters(self):
     return {
      'pluck': pluck,
-     'anywithattr': any_with_attr
+     'anywithattr': any_with_attr,
+     'selectattr': select_attr
     }
